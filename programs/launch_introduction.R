229395a3524081956ff03fa7c0c9172810e5e50a
Méthodo <methode-stat@douane.finances.gouv.fr>
2024-07-03 16:24:46 +0200

    fix merge conflicts

--- programs/launch_introduction.R
+++ programs/launch_introduction.R
@@ -1,12 +1,3 @@
-endogenous_intro <- import_endogenous(input_object, "I")
-
-exogenous_intro <- import_ca3(base_CA3 = base_CA3,
-                              sample_intro = sample_intro,
-                              delete_data = delete, 
-                              date_prediction = date_prediction)
-
-detail_intro <- import_detail(input_object, "I", "payp %notin% c('XU', 'GB')")
-
 
 ### Create introduction object
 
