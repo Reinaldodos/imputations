994acd4422c59211b0efbd3c9caa73a11531efd4
lea_DSECE <lea.chabanon@douane.finances.gouv.fr>
2024-07-09 11:54:49 +0200

    appeler dans l'environnement global

--- programs/launch_request.R
+++ programs/launch_request.R
@@ -19,13 +19,9 @@ my_bdd <-
 # lecture lazy
 data <- tbl(my_bdd, in_schema("sc_astrineo", "florea"))
 
-debut = as.Date("2023-01-01")
-
-fin=as.Date("2024-04-01")
-
-period=as.character(year(seq(debut,fin,by="year")))
-
+# fonction requete
 GroupFiltreSum <- function(data,
+                           fichier,
                             grouping_var,
                             column_name,
                             type,
@@ -47,81 +43,80 @@ GroupFiltreSum <- function(data,
            imex %in% flux,
            oblig %in% type,
            vaco %in% td,
-           sire == "531597128") %>% 
+           sire %in% c("056802218","056806813","056807290","056809957","531597128") #pour test
+           ) %>% 
     group_by(
     across({{grouping_var}})) %>%  
     summarise(across({{column_name }}, \(x) sum(x, na.rm = TRUE))) %>% 
+    rename(siren = sire) %>% 
     collect()
+  
+  # saveRDS(table,paste0(fichier,".rds")) #enregistrer ou pas, dans ETL ?
   
   return(table)
 }
 
+# Parmètres des requetes
 
-# test = groupFiltre_sum(
-#   data = data,
-#   grouping_var = c("sire", "adep", "mdep", "imex","regdem","oblig","vaco"),
-#   column_name = c("vart","vfte"),
-#   type = c("1","4"),
-#   flux = c("3","4"),
-#   td = c("1","3"),
-#   annee = period,
-#   cniv = TRUE
-# )
-
-
-RequeteParams = function(debut, fin) {
+RequeteParams = function(date_ref) {
   ER_exped = list(
+    fichier = "ER_exped",
     grouping_var = c("sire", "adep", "mdep", "regdem"),
     column_name = c("vart", "vfte"),
     type = c("4"),
     flux = c("4"),
     td = c("0"),
-    annee = as.character(year(seq(debut, fin, by = "year"))),
+    annee = as.character(year(seq(date_ref - months(2), date_ref, by = "year"))),
     cniv=FALSE
   )
   exped_imput = list(
+    fichier = "exped_imput",
     grouping_var = c("sire", "adep", "mdep","regdem"),
     column_name = c("vart"),
     type = c("1"),
     flux = c("4"),
     td = c("1", "3"),
-    annee = c("2024", "2023"),
+    annee = as.character(year(seq(date_ref - months(48), date_ref, by = "year"))),
     cniv=FALSE
   )
   exped_ventil = list(
+    fichier = "exped_ventil",
     grouping_var = c( "sire", "adep", "mdep", "a129", "nc8", "payp", "pyod", "dept", "regdem", "temo", "natr", "conf"),
     column_name = c("vart"),
     type = c("1"),
     flux = c("4"),
     td = c("1", "3"),
-    annee = c("2024", "2023"),
+    annee = as.character(year(seq(date_ref - months(24), date_ref, by = "year"))),
     cniv=FALSE
   )
   intro_imput = list(
+    fichier = "intro_imput",
     grouping_var = c("sire", "adep", "mdep"),
     column_name = c("vart"),
     type = c("1"),
     flux = c("3"),
     td = c("1", "3"),
-    annee = c("2024", "2023"),
+    annee = as.character(year(seq(date_ref - months(48), date_ref, by = "year"))),
     cniv=FALSE
   )
   intro_ventil = list(
+    fichier = "intro_ventil",
     grouping_var = c( "sire", "adep", "mdep", "a129", "nc8", "payp", "pyod", "dept", "regdem", "temo", "natr", "conf"),
     column_name = c("vart"),
     type = c("1"),
     flux = c("3"),
     td = c("1", "3"),
-    annee = c("2024", "2023"),
+    annee = as.character(year(seq(date_ref - months(24), date_ref, by = "year"))),
     cniv=FALSE
   )
   vin_spiritueux = list(
+    fichier = "vin_spiritueux",
     grouping_var = c( "sire", "adep", "mdep", "ngp", "nc8"),
     column_name = c("vart","usup"),
     type = c("1","4"),
     flux = c("1","2","3","4"),
     td = c("1", "3"),
-    annee = c("2024", "2023"),
+    annee = as.character(year(seq(date_ref - months(12), date_ref, by = "year"))),
     cniv = TRUE
   )
   
@@ -135,10 +130,13 @@ RequeteParams = function(debut, fin) {
   return (meta_liste)
 }
 
-liste_requete = RequeteParams(debut=debut,fin=fin)
+date_ref= as.Date("2024-04-01")
+
+liste_requete = RequeteParams(date_ref = date_ref)
 
 mes_requetes = liste_requete %>% 
   map( ~ GroupFiltreSum(
+           fichier =.$fichier,
            annee = .$annee,
            flux = .$flux,
            type = .$type,
@@ -148,167 +146,5 @@ mes_requetes = liste_requete %>%
            cniv = .$cniv,
     data = data)) 
 
+list2env(mes_requetes, envir = .GlobalEnv)
 
-
-
-cible_test = echantillon %>% filter(siren %in% c("056802218","056806813","056807290","056809957")) %>% distinct(siren)
-
-
-liste_fichiers <- data.frame(
-  type = c(
-    "intro_imput",
-    "intro_ventil",
-    "exped_imput",
-    "exped_ventil",
-    "ER_exped",
-    "vin-spiritueux"
-  ),
-  start = c(
-    date_ref - months(48),
-    date_ref - months(24),
-    date_ref - months(48),
-    date_ref - months(24),
-    date_ref - months(2),
-    date_ref - months(12)
-  ),
-  end = date_ref) %>% 
-  rowwise() %>% mutate(command =
-  map( .,.f= WriteCommand(start, end, (str_split(type, "_20") %>% flatten_chr())[1])))
-  
-
-request_data <-  bind_rows(
-  intro_imput_file %>%
-    subset(subset = !historical, select = -c(historical, astrineo_input, skiprows, encoding, dec)), 
-  exped_imput_file %>%
-    subset(subset = !historical, select = -c(historical, astrineo_input, skiprows, encoding, dec)), 
-  intro_ventil_file %>% select(-astrineo_input, skiprows, encoding, dec), 
-  exped_ventil_file %>% select(-astrineo_input, skiprows, encoding, dec),
-  ER_file %>% select(-c(skiprows, encoding, dec)), 
-  cniv_file %>% subset(subset = (type == "input"), select = c(directory, files, start, end))
-) %>% 
-  rowwise() %>%
-  mutate(
-    command = WriteCommand(start, end, (str_split(files, "_20") %>% flatten_chr())[1])
-  ) %>%
-  ungroup()
-
-
-
-dta = data %>%
-  filter (sire %in% cible_liste,
-          adep=="2024",
-          mdep %in% v) %>% 
-  group_by(sire,adep, mdep) %>%
-  summarise(n = sum(vart, na.rm = T)) %>%
-  collect()
-
-
-echantillon = sample %>% filter(date_beg=="2024-01-01")
-delete = delete %>% filter(!(is.na(siren_repreneur)))
-
-cible = echantillon %>% distinct(siren) %>% 
-  bind_rows(delete %>% distinct(siren)) %>% 
-  bind_rows(delete %>% distinct(siren=siren_repreneur)) %>% unique()
-
-cible_liste = c(unique(cible$siren))
-
-
-
-
-
-start_time=Sys.time()
-dta = data %>%
-  filter (sire %in% cible_liste,
-          adep=="2024",
-          mdep %in% v) %>% 
-  group_by(sire,adep, mdep) %>%
-  summarise(n = sum(vart, na.rm = T)) %>%
-  collect()
-print(Sys.time() - start_time)
-
-
-
-cible_test = echantillon %>% filter(siren %in% c("056802218","056806813","056807290","056809957")) %>% distinct(siren)
-
-
-
-WriteCommand <- function(from, to, type){
-  period <- seq.Date(from = from, to = to, by = 'month') %>%
-    as_tibble() %>%
-    mutate(adep = format(value, "%Y"), 
-           mdep = format(value, "%m")) %>%
-    group_by(adep) %>%
-    mutate(month_command = sprintf("mdep in (%s)", paste("'", mdep, "'", collapse = ", ", sep = ""))) %>%
-    ungroup() %>%
-    group_by(month_command) %>%
-    summarise(command = sprintf("((adep in (%s)) and (%s))", 
-                                paste("'", unique(adep), "'", collapse = ", ", sep = ""), 
-                                month_command)) %>%
-    ungroup() %>%
-    select(command) %>%
-    unique() %>%
-    flatten_chr() %>%
-    paste("(", ., ")", collapse = " or ", sep = "")
-  
-  dict <- data.frame(
-    type = c("intro_imput", "intro_ventil", "exped_imput", "exped_ventil", "ER_exped", "vin-spiritueux"), 
-    var = c("sire, adep, mdep", 
-            "sire, adep, mdep, a129, nc8, payp, pyod, dept, regdem, temo, natr, conf", 
-            "sire, adep, mdep, regdem", 
-            "sire, adep, mdep, a129, nc8, payp, pyod, dept, regdem, temo, natr, conf", 
-            "sire, adep, mdep, regdem", 
-            "sire, adep, mdep, ngp, nc8, imex, case when imex in ('1', '3') then 'I' else 'E' end flux"),
-    var_group_by = c("sire, adep, mdep", 
-                     "sire, adep, mdep, a129, nc8, payp, pyod, dept, regdem, temo, natr, conf", 
-                     "sire, adep, mdep, regdem", 
-                     "sire, adep, mdep, a129, nc8, payp, pyod, dept, regdem, temo, natr, conf", 
-                     "sire, adep, mdep, regdem", 
-                     "sire, adep, mdep, ngp, nc8, imex, case when imex in ('1', '3') then 'I' else 'E' end"),
-    agg_var = c("vart", "vart", "vart", "vart", "vart, vfte", 
-                "vart, usup") 
-  ) %>%
-    mutate(condition = case_when(
-      (grepl(pattern = "intro", x = type)) ~ "(imex = '3') and (oblig = '1') and (vaco in ('1', '3'))", 
-      (grepl(pattern = "imput", x = type) | grepl(pattern = "ventil", x = type)) ~ "(imex = '4') and (oblig = '1') and (vaco in ('1', '3'))", 
-      (grepl(pattern = "ER", x = type)) ~ "(oblig = '4') and (regdem = '21')", 
-      TRUE ~ "((nc8 like '2204%') or (nc8 like '2208%')) and (vaco in ('1','3'))"
-    ))
-  var <- dict[dict$type == type,]$var
-  var_group_by <- dict[dict$type == type,]$var_group_by
-  agg_var <- dict$agg_var %>% str_split(", ") %>% flatten_chr()
-  condition <- dict[dict$type == type,]$condition
-  command <- sprintf(
-    "select %s, %s from sc_astrineo.florea where ((%s) and %s) group by %s", 
-    var, 
-    paste(sprintf("sum(%s) %s", agg_var, agg_var), collapse = ", "), 
-    period, 
-    condition, 
-    var_group_by
-  )
-  return(command)
-}
-
-
-
-
-liste_fichiers <- data.frame(
-  type = c(
-    "intro_imput",
-    "intro_ventil",
-    "exped_imput",
-    "exped_ventil",
-    "ER_exped",
-    "vin-spiritueux"
-  ),
-  start = c(
-    date_ref - months(48),
-    date_ref - months(24),
-    date_ref - months(48),
-    date_ref - months(24),
-    date_ref - months(2),
-    date_ref - months(12)
-  ),
-  end = date_ref) %>% 
-  rowwise() %>% mutate(command =
-  map( .,.f= WriteCommand(start, end, (str_split(type, "_20") %>% flatten_chr())[1])))
-  
