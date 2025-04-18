01e5f6e774e96f6b4a8bfb885b71475bf052a9d2
Méthodo <methode-stat@douane.finances.gouv.fr>
2025-04-18 11:46:06 +0200

    methode reglin iso

--- les_modeles.R
+++ les_modeles.R
@@ -233,307 +233,130 @@ taking_mean(
 ## Le point calculé par regression linéaire -----------------------------------------------------------
 
 # launch_reglin
-# code Bao
 
-setGeneric(
-  name = "create_ts",
-  def = function(object,
-                 data,
-                 months = c('july', 'august1',
-                            'august2', 'september')) {
-    standardGeneric("create_ts")
+create_ts = function(data,
+                     months = c('july', 'august1',
+                                'august2', 'september')) {
+  ttmp <- cbind(ts_ts(data[, c('period', endo_name)]),
+                ts_ts(data[, c('period', exog_name)]))
+  for (month in months) {
+    ttmp <- cbind(ttmp, ts_ts(data[, c('period', month)]))
   }
-)
-setMethod(
-  f = "create_ts",
-  signature = "NR",
-  definition = function(object,
-                        data,
-                        months = c('july', 'august1',
-                                   'august2', 'september')) {
-    ttmp <- cbind(ts_ts(data[, c('period', object@endo_name)]),
-                  ts_ts(data[, c('period', object@exog_name)]))
-    for (month in months) {
-      ttmp <- cbind(ttmp, ts_ts(data[, c('period', month)]))
-    }
-    colnames(ttmp) <-
-      append(c(object@endo_name, object@exog_name), months)
-    ttmp[is.na(ttmp)] <- 0
-    return(ttmp)
-  }
-)
-
-
-setGeneric(
-  name = "launch_reglin",
-  def = function(object,
-                 prediction_period,
-                 siren,
-                 nb_learning_year = 5,
-                 nbproc = 10,
-                 alternative = TRUE) {
-    standardGeneric("launch_reglin")
-  }
-)
-setMethod(
-  f = "launch_reglin",
-  signature = "NR",
-  definition = function(object,
-                        prediction_period,
-                        siren,
-                        nb_learning_year = 5,
-                        nbproc = 10,
-                        alternative = TRUE) {
-    pred_period <- seq(
-      from = min(prediction_period),
-      to = max(max(prediction_period), min(prediction_period) + months(12)),
-      by = 'month'
-    )
-    cl <- makeCluster(nbproc)
-    registerDoParallel(cl)
-    start_time <- Sys.time()
-    prediction <- foreach(
-      siren = siren,
-      .combine = rbind,
-      .export = 'create_ts',
-      .packages = c('dplyr', 'lubridate', 'tsbox')
-    ) %dopar% {
-      data <- (
-        object@endogenous
-        %>% left_join(object@exogenous,
-                      by = c('siren', 'period'))
-        %>% .[.$siren == siren
-              & .$period <= max(pred_period)
-              &
-                .$period >= (ymd(min(pred_period)) - years(nb_learning_year)), ]
-        %>% full_join(data.frame(period = pred_period),
-                      by = 'period')
-        %>% mutate(
-          year = year(period),
-          july = case_when(((
-            month(period) == 7
-          )
-          & ((UQ(sym(
-            object@exog_name
-          )) == 0
-          |
-            is.na(UQ(
-              sym(object@exog_name)
-            )))
-          )) ~ 1,
-          TRUE ~ 0),
-          august1 = case_when(((
-            month(period) == 8
-          )
-          &
-            ((UQ(sym(
-              object@exog_name
-            ))[month(period) == 7] == 0)
-            |
-              is.na(UQ(sym(
-                object@exog_name
-              ))[month(period) == 7])
-            )) ~ 1,
-          TRUE ~ 0),
-          august2 = case_when(((
-            month(period) == 8
-          )
-          & ((UQ(sym(
-            object@exog_name
-          )) == 0) |
-            is.na(UQ(sym(
-              object@exog_name
-            )))
-          )) ~ 1,
-          TRUE ~ 0),
-          september = case_when(((
-            month(period) == 9
-          )
-          &
-            ((UQ(sym(
-              object@exog_name
-            ))[month(period) == 8] == 0)
-            |
-              is.na(UQ(sym(
-                object@exog_name
-              ))[month(period) == 8])
-            )) ~ 1,
-          TRUE ~ 0)
-        )
-      )
-      
-      print(data)
-      data <- create_ts(object, data)
-      model <- try(lm(as.formula(paste(
-        object@endo_name,
-        paste(
-          c(
-            object@exog_name,
-            'july',
-            'august1',
-            'august2',
-            'september'
-          ),
-          collapse = '+'
-        ),
-        sep = '~'
-      )),
-      data = window(data, end = c(
-        year(min(pred_period) - months(1)),
-        month(min(pred_period) -
-                months(1))
-      ))),
-      silent = TRUE)
-      if (!inherits(model, 'try-error')) {
-        model <- try(step(model, direction = 'backward'),
-                     silent = TRUE)
-        if (!inherits(model, 'try-error')) {
-          pred <- data.frame(
-            siren = rep(siren, length(pred_period)),
-            period = pred_period,
-            prediction = predict(model, newdata = window(data, start = c(
-              year(min(pred_period)),
-              month(min(pred_period))
-            )))
-          )
-        } else{
-          pred <- data.frame(
-            siren = rep(siren, length(pred_period)),
-            period = pred_period,
-            prediction = NA
-          )
-        }
-      } else{
-        pred <- data.frame(
-          siren = rep(siren, length(pred_period)),
-          period = pred_period,
-          prediction = NA
-        )
-      }
-    }
-    # unregister_dopar()
-    stopCluster(cl)
-    time_delta <- Sys.time() - start_time
-    prediction <-
-      mutate(prediction, method = "launch_reglin")
-    # print(sprintf("Temps d'ex?cution est %f %s", time_delta, units(time_delta)))
-    if (alternative) {
-      new_prediction <- taking_last_year(
-        object = object,
-        prediction_period = prediction_period,
-        siren = unique(prediction[is.na(prediction$prediction)
-                                  |
-                                    prediction$prediction <= 0, ]$siren),
-        alternative = TRUE
-      )
-      prediction <- bind_rows(
-        x = inner_join(prediction[c('siren', 'period')], new_prediction, by = c('siren', 'period')),
-        y = anti_join(prediction, new_prediction[c('siren', 'period')], by =
-                        c('siren', 'period'))
-      )
-    }
-    return(as_tibble(prediction[prediction$period %in% prediction_period,
-                                c('siren', 'period', 'prediction', 'method')]))
-  }
-)
+  colnames(ttmp) <-
+    append(c(endo_name, exog_name), months)
+  ttmp[is.na(ttmp)] <- 0
+  return(ttmp)
+}
 
 
 
-##  fonction
-
-launch_reglin <- function(endogenous, exogenous, 
-                          prediction_period, siren_list,
+launch_reglin <- function(endogenous,
+                          exogenous,
+                          prediction_period,
+                          siren_list,
                           nb_learning_year = 5) {
-  library(dplyr)
-  library(lubridate)
-  library(foreach)
-  library(doParallel)
-  library(tibble)
+  pacman::p_load(tsbox)
   
   pred_period <- seq(
     from = min(prediction_period),
     to = max(max(prediction_period), min(prediction_period) + months(12)),
-    by = "month"
+    by = 'month'
   )
+  data <-
+    endogenous %>%
+    left_join(exogenous,
+              by = c('siren', 'period')) %>%
+    filter(siren == siren_list,
+           period <= max(pred_period),
+           period >= (ymd(min(pred_period)) - years(nb_learning_year)))
   
-  endo_name = "vart"
-  exog_name="medoc_0031"
- 
-    data <- endogenous |>
-      left_join(exogenous, by = c("siren", "period")) |>
-      filter(
-        siren == siren_list,
-        period <= max(pred_period),
-        period >= ymd(min(pred_period)) - years(nb_learning_year)
-      ) |>
-      full_join(tibble(period = pred_period), by = "period") |>
-      arrange(period) |>
-      mutate(
-        year = year(period),
-        lag_exog = lag(.data[[exog_name]]),
-        july = if_else(month(period) == 7 &
-                         (is.na(.data[[exog_name]]) |
-                            .data[[exog_name]] == 0), 1, 0),
-        august1 = if_else(month(period) == 8 &
-                            (is.na(lag_exog) |
-                               lag_exog == 0), 1, 0),
-        august2 = if_else(month(period) == 8 &
-                            (is.na(.data[[exog_name]]) |
-                               .data[[exog_name]] == 0), 1, 0),
-        september = if_else(month(period) == 9 &
-                              (is.na(lag_exog) |
-                                 lag_exog == 0), 1, 0)
+  data2 <- data %>%
+    full_join(data.frame(period = pred_period),
+              by = 'period') %>%
+    mutate(
+      year = year(period),
+      july = case_when((
+        month(period) == 7 & (medoc_0031 == 0 | is.na(medoc_0031))
+      ) ~ 1,
+      TRUE ~ 0),
+      august1 = case_when((month(period) == 8 &
+                             ((lag(medoc_0031) == 0) | is.na(lag(medoc_0031))
+                             )) ~ 1,
+                          TRUE ~ 0),
+      august2 = case_when((month(period) == 8 &
+                             ((medoc_0031 == 0) | is.na(medoc_0031)
+                             )) ~ 1,
+                          TRUE ~ 0),
+      september = case_when((month(period) == 9 &
+                               ((lag(medoc_0031) == 0) | is.na(lag(medoc_0031))
+                               )) ~ 1,
+                            TRUE ~ 0)
+    )
+  
+  
+  print(data)
+  data <- create_ts(data2)
+  model <- try(lm(as.formula(paste('vart',
+                                   paste(
+                                     c('medoc_0031',
+                                       'july',
+                                       'august1',
+                                       'august2',
+                                       'september'),
+                                     collapse = '+'
+                                   ),
+                                   sep = '~')),
+                  data = window(data, end = c(
+                    year(min(pred_period) - months(1)),
+                    month(min(pred_period) -
+                            months(1))
+                  ))),
+               silent = TRUE)
+  if (!inherits(model, 'try-error')) {
+    model <- try(step(model, direction = 'backward'),
+                 silent = TRUE)
+    if (!inherits(model, 'try-error')) {
+      pred <- data.frame(
+        siren = rep(siren_list, length(pred_period)),
+        period = pred_period,
+        prediction = predict(model, newdata = window(data, start = c(
+          year(min(pred_period)),
+          month(min(pred_period))
+        )))
       )
-    
-    data_ts <- data |>
-      mutate(across(c(vart,medoc_0031), ~ replace_na(., 0))) |>
-      arrange(period)
-    
-    model_data <- data_ts |>
-      filter(period < min(pred_period))
-    
-    model <- tryCatch({
-      lm(formula = as.formula(paste(endo_name, "~", paste(
-        c(exog_name, "july", "august1", "august2", "september"),
-        collapse = " + "
-      ))),
-      data = model_data) |>
-        step(direction = "backward")
-    },
-    error = function(e)
-      NULL)
-    
-    future_data <- data_ts |>
-      filter(period %in% pred_period)
-    
-    pred <- if (!is.null(model)) {
-      tryCatch({
-        predict(model, newdata = future_data) |>
-          as.numeric()
-      },
-      error = function(e)
-        rep(NA_real_, length(pred_period)))
-    } else {
-      rep(NA_real_, length(pred_period))
+    } else{
+      pred <- data.frame(
+        siren = rep(siren_list, length(pred_period)),
+        period = pred_period,
+        prediction = NA
+      )
     }
-    
-    prediction =  tibble(siren = siren_list,
-                         period = pred_period,
-                         prediction = pred)
-    
-    
-    prediction = prediction |>
-      filter(period %in% prediction_period) |>
-      mutate( method = "launch_reglin") |>
-      select(siren, period, prediction, method)
-    
-    return(prediction)
+  } else{
+    pred <- data.frame(
+      siren = rep(siren_list, length(pred_period)),
+      period = pred_period,
+      prediction = NA
+    )
+  }
+  
+  prediction <- pred %>%
+    filter(period %in% prediction_period) %>%
+    mutate(prediction, method = "launch_reglin")
+  return(prediction)
 }
+
 
 launch_reglin(
   endogenous = endogenous_intro,
   exogenous = exogenous_intro,
   prediction_period = as.Date("2024-12-01"),
   siren_list = "328358734",
+  nb_learning_year = 5
+)
+launch_reglin(
+  endogenous = endogenous_intro,
+  exogenous = exogenous_intro,
+  prediction_period = as.Date("2024-12-01"),
+  siren_list = "300000000",
   nb_learning_year = 5
 )
