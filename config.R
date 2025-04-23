abe5dfb42fa8d8ff696d285fa8634c9adde5c435
Méthodo <methode-stat@douane.finances.gouv.fr>
2025-04-23 16:31:49 +0200

    date_ref

--- config.R
+++ config.R
@@ -11,7 +11,7 @@ c(
 
 # DATE ------------------------------------------------------
 
-date_ref <- as_date("2025-02-01")
+date_ref <- as_date("2025-03-01")
 nb_date_prediction <- 2
 first_publication_date <- as_date("2022-01-01")
 date_prediction <- seq.Date(
