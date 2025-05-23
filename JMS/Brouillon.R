6d3ae3accb2943a999f5a481cfa59b0d6f975c79
Méthodo <methode-stat@douane.finances.gouv.fr>
2025-05-23 11:56:11 +0200

    brouillon

--- JMS/Brouillon.R
+++ JMS/Brouillon.R
@@ -1,21 +1,24 @@
-Erreurs <- 
-  output_test %>% 
-  mutate(is_error = map(.x = error,.f = is.null)) %>% 
-  unnest(cols = c(is_error)) 
+Erreurs <-
+  output_test %>%
+  mutate(is_error = map(.x = error, .f = is.null)) %>%
+  unnest(cols = c(is_error))
 
-Erreurs %>% 
-  count(!is_error, model, flux, regdem)
+Erreurs %>%
+  count(echec_modele = !is_error, model, flux, regdem) %>%
+  spread(echec_modele, n, fill = 0) %>%
+  janitor::adorn_percentages() %>%
+  janitor::adorn_pct_formatting(affix_sign = TRUE)
 
-Erreurs %>% 
-  filter(!is_error) %>% 
-  split(x = .$error, f = paste(.$siren, .$model, sep = " : ")) 
+Erreurs %>%
+  filter(!is_error) %>%
+  split(x = .$error, f = paste(.$siren, .$model, sep = " : "))
 
 pluck_pull <- function(string) {
   pluck(.x = test, string, 1)
 }
-test <- 
-  output_test %>% 
-  filter(model == "taking_exog") %>% 
+test <-
+  output_test %>%
+  filter(model == "taking_exog") %>%
   sample_n(1)
 
 taking_exog(
@@ -26,18 +29,17 @@ taking_exog(
 )
 
 
-sample %>% 
-  dplyr::count(date_beg) %>% 
-  filter(date_beg<date_a_predire) %>% 
-  top_n(n = 1, wt = date_beg) %>% 
+sample %>%
+  dplyr::count(date_beg) %>%
+  filter(date_beg < date_a_predire) %>%
+  top_n(n = 1, wt = date_beg) %>%
   semi_join(x = sample)
 
-exogenous %>% 
-  dplyr::count(flux, period) %>% 
-  spread(flux, n) %>% 
+exogenous %>%
+  dplyr::count(flux, period) %>%
+  spread(flux, n) %>%
   data.frame()
 
-input %>% 
-  filter(model == "launch_reglin") %>% 
+input %>%
+  filter(model == "launch_reglin") %>%
   select(siren, flux, regdem, exogenous)
-
